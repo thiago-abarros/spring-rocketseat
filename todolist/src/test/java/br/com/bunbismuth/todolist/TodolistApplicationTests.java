37bab645291a26fe58bf1090a9dfc8bd1c2ead43
Thiago Barros <bunbismuth@proton.me>
2023-10-14 16:56:13 -0300

    Create: Initial config
    All the files in the initial spring package.

---
+++ todolist/src/test/java/br/com/bunbismuth/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bunbismuth.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
